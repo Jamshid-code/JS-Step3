bfb3e132eb4d8246ed7b6a9f88622159e5ad9dab
Jamshid <jamshidazizov0411@gmail.com>
2025-01-17 20:16:29 +0300

    Renew

--- main.js
+++ main.js
@@ -1,21 +1,50 @@
-let number = prompt('Введите число:')
-for(var i = 0; i < 1; i++){
-    if(number == 1){
-        alert(' 1 banana')
+
+let number = +prompt('Введите число')
+let i = 1
+while ( i <= number) {
+    if( i == 1){
+        console.log(i + ' banana');
+
     }else{
-        alert(number + ' bananas')
+        console.log( i + ' bananas');
+
     }
+    i++
 }
 
-let number1 = +prompt('Введите число')
-for(i=0; i<=number1; i++){
-    if(i % 2 && i<=number1){
-        console.log('Четные числа до ' + number1 );
-    }
+
+
+let number1 = +prompt("Сколько раз сработает цикл");
+let summ = 0;
+
+for (let i = 0; i < number1; i++) {
+  if (i % 2 == 0) {
+    summ += i;
+  }
 }
+console.log('Сумма четных чисел ' + summ);
+
 
 let firstnumber = +prompt('Введите число')
 let degree = +prompt('Введите степень')
 for( i = 0; i < 1; i++ ){
-    alert( ' Ответ ' + firstnumber ** degree )
+    console.log( firstnumber ** degree )
 }
+
+
+
+    // let number = prompt('Введите число:')
+    // for(var i = 0; i < 1; i++){
+    //     if(number == 1){
+    //         alert(' 1 banana')
+    //     }else{
+    //         alert(number + ' bananas')
+    //     }
+    // }
+    
+    // let number1 = +prompt('Введите число')
+    // for(i=0; i<=number1; i++){
+    //     if(i % 2 && i<=number1){
+    //         console.log('Четные числа до ' + number1 );
+    //     }
+    // }
